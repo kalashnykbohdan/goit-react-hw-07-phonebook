21f8612a6d7cefb1610870a5d3ada2a3d0a660ef
Kalashnyk Bohdan <space.bohdan@gmail.com>
2020-07-31 23:08:28 +0300

    Updates

---
+++ precache-manifest.05ed78264c99b4c59b4ff5a21443a3db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d649d37d23ff6b09274943b26ef854b",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "0f20004140012ba2d440",
+    "url": "/goit-react-hw-07-phonebook/static/css/2.2cf0c88b.chunk.css"
+  },
+  {
+    "revision": "395d10ee6418ef213a32",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.7818102a.chunk.css"
+  },
+  {
+    "revision": "0f20004140012ba2d440",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.96808cbf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.96808cbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "395d10ee6418ef213a32",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.135013ee.chunk.js"
+  },
+  {
+    "revision": "e3f60b8770ae85b5a663",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.6ec4936c.js"
+  }
+]);
